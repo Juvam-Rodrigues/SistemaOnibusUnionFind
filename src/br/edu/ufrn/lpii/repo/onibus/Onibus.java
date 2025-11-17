9dcf7ac5d61a43e5705236dfec9aa3792a70da05
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2025-11-17 12:33:08 -0300

    Criação dos atributos do ônibus: nome e rota, com seus respectivos métodos. Além do construtor.

--- src/br/edu/ufrn/lpii/repo/onibus/Onibus.java
+++ src/br/edu/ufrn/lpii/repo/onibus/Onibus.java
@@ -1,5 +1,36 @@
 package br.edu.ufrn.lpii.repo.onibus;
 
+import br.edu.ufrn.lpii.repo.union_find.Rota;
+
 public class Onibus {
+	private String nome;
+	private Rota rota;
+	
+	public Onibus(String nome, Rota rota) {
+		super();
+		this.nome = nome;
+		this.rota = rota;
+	}
+
+	public Onibus() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Rota getRota() {
+		return rota;
+	}
+
+	public void setRota(Rota rota) {
+		this.rota = rota;
+	} 
+	
 	
 }
